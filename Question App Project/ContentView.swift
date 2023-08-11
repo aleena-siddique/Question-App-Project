552a5bd4ae111d18828e5ec3024c06baebf8e595
Aleena Siddique <aaleena.siddique@icloud.com>
2023-08-11 11:37:19 -0400

    Initial Commit

---
+++ Question App Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Question App Project
+//
+//  Created by Aleena Siddique on 2023-08-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Question App Project/Question_App_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Question_App_ProjectApp.swift
+//  Question App Project
+//
+//  Created by Aleena Siddique on 2023-08-11.
+//
+
+import SwiftUI
+
+@main
+struct Question_App_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
